868db911c2d2bb24abc2f40b4f59e2fbfdedb46f
Your Name <you@example.com>
2019-09-24 20:17:27 +0000

    peer programmed with nmehanma

---
+++ spinner2.js
@@ -0,0 +1,14 @@
+// peer programmed with Najeam
+
+let char = "|/-\|/-\|"
+
+let time = 100
+
+for (let i = 0; i < char.length; i++) {
+
+  setTimeout(() => {process.stdout.write(`\r${char[i]}    `)}, time)
+  time += 200;
+  if (time === 100 + char.length * 200) {
+    setTimeout(() => {process.stdout.write(`\r${char[i]}\n`)}, time)
+  }
+};
